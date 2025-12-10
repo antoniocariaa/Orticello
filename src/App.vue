dd15f9a2267e5225b5d17290b734b0fc89290e24
Antonio Caria <antonio.caria-1@studenti.unitn.it>
2025-12-10 16:26:00 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  // import tailwind styles and daisy ui
+  import "tailwindcss/tailwind.css";
+  import "daisyui";
+</script>
+
+<template>
+  <!-- create a box which contains a button -->
+  <div class="box">
+    <button class="btn btn-primary">Test Button</button>
+  </div>
+  <h1>You did it!</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+import daisyui from 'daisyui';
+export default { content: ["./src/**/*.{html,js,vue}"],
+theme: { extend: {} },
+plugins: [ daisyui ] }
+
